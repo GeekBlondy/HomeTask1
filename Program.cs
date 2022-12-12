ef7dc36cbd4d833de2ee0e018702f8b9a65d5989
GeekBlondy <119070290+GeekBlondy@users.noreply.github.com>
2022-12-12 20:21:22 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ Console.WriteLine("Input a number B: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input a number C: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
-int max = num2;
+int max = num1;
 
 if (num2 > max) max = num2;
 if (num3 > max) max = num3;
